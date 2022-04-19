2ef3c8b106ba8162598f229051266da7cf033335
Hiroaki Takase <hitakase@c1r25s4.42tokyo.jp>
2022-04-19 22:36:05 +0900

    lastlast

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hitakase </var/mail/hitakase>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 18:28:59 by hitakase          #+#    #+#             */
-/*   Updated: 2022/04/19 18:29:01 by hitakase         ###   ########.fr       */
+/*   Updated: 2022/04/19 22:23:14 by hitakase         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,6 @@ void	*ft_calloc(size_t n, size_t size)
 	p = malloc(n * size);
 	if (p == NULL)
 		return (NULL);
-	ft_bzero((unsigned char *)p, n * size);
-	return ((void *)p);
+	ft_bzero(p, n * size);
+	return (p);
 }
